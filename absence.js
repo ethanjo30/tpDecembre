b56b9a76e009de8cbb5f6d9f599da4990030e661
ethanjo30 <ethan.jorand24@gmail.com>
2023-12-19 16:34:10 +0100

    modif du 19/12/23

--- absence.js
+++ absence.js
@@ -39,108 +39,22 @@ for(let i =0; i < btnRadio.length; i++) {
 }
 
 
-let valider = document.querySelector("valider");
-    valider.addEventListener("click", (event) => {
-        let dateChoisi = document.getElementById(dateHeure)
-            dateChoisi.addEventListener("change", (event) => {
-                dateChoisi = event.target
-            })
-            console.log(dateChoisi)
-    })
-console.log(valider)
+fetch("absence.json").then((response) => response.json())
+.then((json) => {for(let j =0; j < json.Motif.length; j++) {
+    let afficheJson = json.Motif[j].categorie;
 
-let motif = `
-<fieldset class="option">
-    <legend class="legend"><h2 class="titreH2">Pour le motif</strong></h2></legend>         
-    <section>
-        <p><strong>Code 01 : retard absence non rémunéré</strong></p>
-        <input type="checkbox"/>
-        <label>Retard</label>
-    </section>
-    <hr class="hr2">       
-    <section>
-        <p><strong>Code 04 : Grève Afpa</strong></p>
-        <input type="checkbox"/>
-        <label>Gréve Afpa</label>
-    </section>
-    <hr class="hr2">
-    <section>
-        <p><strong>Code 05 : Maladie</strong></p>
-        <input type="checkbox"/>
-        <label>Maladie</label>
-    </section>
-    <hr class="hr2">
-    <section>
-        <p><strong>Code 06 : Absence Légale</strong></p>
-        <input type="checkbox"/>
-        <label>Naissance ou adoption (3jours)</label><br>
-        <input type="checkbox"/>
-        <label>Mariage (4jours)</label><br>
-        <input type="checkbox"/>
-        <label>Mariage d'un enfant (1jours)</label><br>
-        <input type="checkbox"/>
-        <label>Décès du conjoint ou du partenaire lié par un pacte
-        civil de solidarité ou d'un enfant (2jours)</label><br>
-        <input type="checkbox"/>
-        <label>décès du mère, de la mère, du berau-père, 
-        de la belle-mère, d'un frère ou d'une soeur (1jours)</label><br>
-        <input type="checkbox"/>
-        <label>Visite médicale obligatoire de grossesse</label><br>
-        <input type="checkbox"/>
-        <label>Stagiaire élu menbre du CA d'une caisse 
-        de sécurité sociale</label><br>
-    </section>
-    <hr class="hr2">
-    <section>
-        <p><strong>code 20 - 21 - 30 - 31 - 40 - 41 : Accident</strong></p>
-        <input type="checkbox"/>
-        <label>Accident du travail sans arrêt</label><br>
-        <input type="checkbox"/>
-        <label>Accident du travail avec arrêt</label><br>
-        <input type="checkbox"/>
-        <label>Accident du trajet sans arrêt</label><br>
-        <input type="checkbox"/>
-        <label>Accident du trajet avec arrêt</label><br>
-        <input type="checkbox"/>
-        <label>Accident du travail  hors afpa sans arrêt</label><br>
-        <input type="checkbox"/>
-        <label>Accident du travail hors afpa avec arrêt</label><br>
-    </section>
-    <hr class="hr2">
-    <section>
-        <p><strong>code 98 : Absence autorisé non rémunérée</strong></p>
-        <input type="checkbox"/>
-        <label>Recherche PE</label><br>
-        <input type="checkbox"/>
-        <label>recherche d'un emploi</label><br>
-        <input type="checkbox"/>
-        <label>Visite d'un salon</label><br>
-        <input type="checkbox"/>
-        <label>Enquête en entreprise</label><br>
-        <input type="checkbox"/>
-        <label>Visite d'un autre centre</label><br>
-        <input type="checkbox"/>
-        <label>RDV psychologue du travail</label><br>
-        <input type="checkbox"/>
-        <label>Rendez-vous POLE EMPLOI</label><br>
-        <input type="checkbox"/>
-        <label>Absence pour fête religieuse hors jours fériés légaux</label><br>
-        <input type="checkbox"/>
-        <label>Grève des transports</label><br>
-        <input type="checkbox"/>
-        <label>Intempéries</label><br>
-        <input type="checkbox"/>
-        <label>Rdv avec le conseiller militaire</label><br>
-        <input type="checkbox"/>
-        <label>Recherche de logement, Rdv organisme divers</label><br>
-        <input type="checkbox"/>
-        <label>Recherche de logement, Rdv organisme divers</label><br>
-        <hr class="hr2">
-    <section>
-        <p><strong>code 99 : Absence non autorisé non rémunérée</strong></p>
-    </section>
-</fieldset>
-`;
+let modifCheckbox = document.querySelector(".modifCheckbox")
+modifCheckbox.innerHTML += "<hr class = 'hr2'>" + afficheJson + "<br>"
 
-let main = document.querySelector("main")
-main.innerHTML = motif
+console.log(afficheJson)
+
+for(let k =0; k <json.Motif[j].nom.length; k++) {
+    let affichjeson = json.Motif[j].nom[k]
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "motif"
+    modifCheckbox.innerHTML += checkbox.outerHTML + affichjeson + "<br>";
+}
+    
+}});
+
